198894ea3b3271b2639b5d9f2fc21f7da807366e
Daniel Cotic <d.cotic26@gmail.com>
2017-12-08 14:05:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,1 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
-
-function addElementToBeginningOfArray(array, element){
-  var element = 'foo'
-  const moreArray = [element,...array]
-  return moreArray
-}
-
-function destructivelyAddElementToBeginningOfArray(array, element){
-  var element = "foo"
-  var moreArray = array = [element,...array]
-  return moreArray
-}
