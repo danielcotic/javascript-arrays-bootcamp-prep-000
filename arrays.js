f6605fbbd372d0518d4f26ca5e83d224e0b009e0
Daniel Cotic <d.cotic26@gmail.com>
2017-12-08 14:14:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,6 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+function addElementToBeginningOfArray(array, element){
+  var element = "foo"
+  array = [element,...cities]
+  return array
+}
